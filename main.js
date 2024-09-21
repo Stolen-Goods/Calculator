40612fde011176a3c5078884fb4af0ac1cc088af
Matt <hurley04155@gmail.com>
2024-09-21 16:15:25 -0400

    Add more JS functions

--- main.js
+++ main.js
@@ -2,20 +2,44 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-function add(firstNumber, secondNumber) {
+function add() {
     return firstNumber + secondNumber;
 }
 
-function subtract(firstNumber, secondNumber) {
+function subtract() {
     return firstNumber - secondNumber;
 }
 
-function multiply(firstNumber, secondNumber) {
+function multiply() {
     return firstNumber * secondNumber;
 }
 
-function divide(firstNumber, secondNumber) {
+function divide() {
     return firstNumber / secondNumber;
 }
 
-function operate()
+function displays() {
+
+}
+
+function operate(operator) {
+    if (operator === "+") {
+        return add();
+    } else if (operator === "-") {
+        return subtract();
+    } else if (operator === "*") {
+        return multiply();
+    } else if (operator === "/") {
+        return divide();
+    } else {
+        return "Error";
+    }
+}
+
+
+let display = document.getElementById("display");
+display.textContent = `${firstNumber} ${secondNumber}`;
+
+
+let equals = document.getElementById("equals");
+equals.addEventListener("click", operate);
