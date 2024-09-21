b6007eeeb43b231e5c8fe63e99287eecac68a632
Matt <hurley04155@gmail.com>
2024-09-21 14:46:47 -0400

    Create calculator design and basic JS

---
+++ main.js
@@ -0,0 +1,21 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+function operate()
